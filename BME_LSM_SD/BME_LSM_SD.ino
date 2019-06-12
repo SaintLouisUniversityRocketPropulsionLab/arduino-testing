2e0a4c63e3f40f3a49ab0ba0d6fb5263d6234287
jkloser <john.kloser3@gmail.com>
2019-06-12 14:58:23 -0500

    Finished SD integration

--- BME_LSM_SD/BME_LSM_SD.ino
+++ BME_LSM_SD/BME_LSM_SD.ino
@@ -8,6 +8,11 @@
 LSM9DS1 imu;
 BME280 bme;
 const int chipSelect = 10;
+char fileName[16];
+unsigned int fnum = 1;
+String dataString = "";
+int good = 8;
+int bad = 6;
 
 //I2C setup
 #define LSM9DS1_M 0x1E
@@ -20,78 +25,115 @@ static unsigned long lastPrint = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  pinMode(good, OUTPUT);
+  pinMode(bad, OUTPUT);
+  digitalWrite(good, HIGH);
+  digitalWrite(bad, HIGH);
+  delay(1000);
+  digitalWrite(bad, LOW);
+
   imu.settings.device.commInterface = IMU_MODE_I2C;
   imu.settings.device.mAddress = LSM9DS1_M;
   imu.settings.device.agAddress = LSM9DS1_AG;
   Wire.begin();
   
-  if (!imu.begin()){
-    Serial.println("IMU did not respond.");
-    delay (1000);
-  }
-  if (!bme.beginI2C()){
-    Serial.println("BME did not respond.");
-    delay (1000);
-  }
   if (!SD.begin(chipSelect)){
-    Serial.println("SD card did not respond.");
     while (1);
   }
-  Serial.println("SD card initialized");
+
+  while (!fnum==0){
+    sprintf(fileName, "file_%03d.csv", fnum);
+    if (SD.exists(fileName)==false){
+      break;
+    }
+    fnum++;
+  }
+  File dataFile = SD.open(fileName, FILE_WRITE);
+
+  if (!imu.begin()){
+    dataFile.println("IMU did not respond");
+    digitalWrite(bad, HIGH);
+    delay (1000);
+    digitalWrite(bad, LOW);
+  }
+  if (!bme.beginI2C()){
+    dataFile.println("BME did not respond.");
+    digitalWrite(bad, HIGH);
+    delay (1000);
+    digitalWrite(bad, LOW);
+  }
+  dataFile.close();  
   printHeader();
 }
 
 void loop() {
-  Serial.print(micros());
-  printTemp();
-  printPress();
-  printGyro();
-  printAccel();
-  printMag();
+  File dataFile = SD.open(fileName, FILE_WRITE);
+  dataString = String(micros());
+
+  recordTemp();
+  recordPress();
+  recordAccel();
+  recordGyro();
+  recordMag();
   
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    digitalWrite(good, HIGH);
+  }
+  else{
+    digitalWrite(bad, HIGH);
+    digitalWrite(good, LOW);
+  }  
 }
 
-void printTemp(){
-  Serial.print(", ");
-  Serial.print(bme.readTempC(), 2);
+void recordTemp(){
+  dataString += ",";
+  dataString += String(bme.readTempC(), 2);
 }
 
-void printPress(){
-  Serial.print(", ");
-  Serial.print(bme.readFloatPressure(), 1);
+void recordPress(){
+  dataString += ",";
+  dataString += String(bme.readFloatPressure(), 1);
 }
 
-void printGyro(){
+void recordGyro(){
   imu.readGyro();
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gx), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gy), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gz), 2);
+  dataString += ",";
+  dataString += String(imu.calcGyro(imu.gx), 2);
+  dataString += ",";
+  dataString += String(imu.calcGyro(imu.gy), 2);
+  dataString += ",";
+  dataString += String(imu.calcGyro(imu.gz), 2);
 }
 
-void printAccel(){
+void recordAccel(){
   imu.readAccel();
-  Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.ax), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.ay), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.az), 2);
+  dataString += ",";
+  dataString += String(imu.calcAccel(imu.ax), 2);
+  dataString += ",";
+  dataString +=  String(imu.calcAccel(imu.ay), 2);
+  dataString += ",";
+  dataString += String(imu.calcAccel(imu.az), 2);
 }
 
-void printMag(){
+void recordMag(){
   imu.readMag();
-  Serial.print(", ");
-  Serial.print(imu.calcMag(imu.mx), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcMag(imu.my), 2);
-  Serial.print(", ");
-  Serial.println(imu.calcMag(imu.mz), 2);
+  dataString += ",";
+  dataString += String(imu.calcMag(imu.mx), 2);
+  dataString += ",";
+  dataString += String(imu.calcMag(imu.my), 2);
+  dataString += ",";
+  dataString += String(imu.calcMag(imu.mz), 2);
 }
 
 void printHeader(){
-  Serial.println("Time, Temp, Press, Gx, Gy, Gz, Ax, Ay, Az, Mx, My, Mz");
+  File dataFile = SD.open(fileName, FILE_WRITE);
+  if (dataFile) {
+    dataFile.println("Time,Temp,Press,Gx,Gy,Gz,Ax,Ay,Az,Mx,My,Mz");
+    dataFile.close();
+  }
+  else{
+    digitalWrite(bad, HIGH);
+  }
 }
